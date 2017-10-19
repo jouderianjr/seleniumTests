bc74f1d18c590e11e3421c13c52a4915124b2026
Jouderian Nobre <jouderianjr@gmail.com>
2017-10-19 09:12:21 -0300

    Add test Examples

---
+++ src/test/java/br/unifor/testExamples/SeleniumExamples.java
@@ -0,0 +1,57 @@
+package br.unifor.testExamples;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SeleniumExamples {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void getFirefoxDriver() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+//        driver.close();
+    }
+
+    @Test
+    public void buttonDisabledWhenInputIsEmpty() {
+        driver.get("http://gitelm.surge.sh");
+
+        WebElement searchButton = driver.findElement(By.xpath("//button[text()='Search']"));
+
+        assertFalse(searchButton.isEnabled());
+    }
+
+    @Test
+    public void buttonEnabledWhenInputHasText() {
+        driver.get("http://gitelm.surge.sh");
+
+        WebElement input = driver.findElement(By.className("input"));
+
+        WebElement searchBtn = driver.findElement(By.xpath("//button[text()='Search']"));
+
+        input.sendKeys("jouderianjr");
+
+        searchBtn.click();
+
+        List<WebElement> cards = driver.findElements(By.className("card"));
+
+
+        assertTrue(cards.size() > 0 );
+    }
+}
